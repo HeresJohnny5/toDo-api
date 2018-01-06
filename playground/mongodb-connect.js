28ea2540851716a5d9470027ceaefaa7739c7fdc
John Erickson <ericksonjohn08@hotmail.com>
2018-01-06 14:07:45 -0500

    Initial commit.

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/TodoApp';
+
+MongoClient.connect(url, (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server.');
+	} 
+	
+	console.log('Connected to MongoDB sever.');
+	
+	// db.collection only has one argument which is a string name for the collection you want to insert into the database
+	// method .insertOne lets you insert a new document into your collection
+//	db.collection('Todos').insertOne({ 
+//		text: 'Something to do',
+//		completed: false
+//	}, (err, res) => {
+//		if (err) {
+//			return console.log(`Unable to insert todo: ${err}.`);
+//		}
+//		
+//		// method .ops will stor all of the docs that were inserted
+//		console.log(JSON.stringify(res.ops, undefined, 2));
+//	});
+	
+//	db.collection('Users').insertOne({
+//		name: 'Lena',
+//		age: 33,
+//		location: 'Portland'
+//	}, (err, res) => {
+//		if (err) {
+//			return console.log(`Unable to insert user: ${err}.`);
+//		}
+//		
+//		console.log(JSON.stringify(res.ops, undefined, 2));
+//	});
+	
+	db.close();
+});
