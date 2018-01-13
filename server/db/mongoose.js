63a2f9437446f70902ea3a4cdde67bc86f9c5ff4
John Erickson <ericksonjohn08@hotmail.com>
2018-01-12 22:45:40 -0500

    Add Config Vars.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+var username = 'process.env.DB_USERNAME';
+var password = 'process.env.DB_PASSWORD';
+
+mongoose.connect(`'mongodb://${username}:${password}@ds155577.mlab.com:55577/todoapp-nodejs` || 'mongodb://localhost:27017/TodoApp', {
 	useMongoClient: true
 });
 
