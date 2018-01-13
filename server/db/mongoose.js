346441ba9b43474995738b3180d06cb692cabc65
John Erickson <ericksonjohn08@hotmail.com>
2018-01-13 10:19:27 -0500

    Another attempt to connect to MongoDB.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,9 @@ mongoose.Promise = global.Promise;
 var username = process.env.DB_USERNAME;
 var password = process.env.DB_PASSWORD;
 
+console.log(username);
+console.log(password);
+
 mongoose.connect("mongodb://" + username + ":" + password + "@ds155577.mlab.com:55577/todoapp-nodejs" || 'mongodb://localhost:27017/TodoApp', {
 	useMongoClient: true
 });
